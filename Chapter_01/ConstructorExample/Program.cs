259f80d6b2fc0123811c3390568c9414573014a3
Mohit rao <mmohitrrao96@gmail.com>
2025-10-30 20:56:00 +0530

    Ex1_2,3,4,5 and ConstructorExamaple added

---
+++ Chapter_01/ConstructorExample/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+namespace ConstructorExample
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            //Without constructor
+            Employee Emp1 = new Employee();
+            Emp1.SetEmployee(12233, 24, "Mohit");
+            Emp1.GetEmployee();
+
+            Employee Emp2 = new Employee();
+            Emp2.SetEmployee(12133, 24, "Rohit");
+             Emp2.GetEmployee();
+
+            Employee Emp3 = new Employee();
+            Emp3.SetEmployee(12233, 34, "Lohit");
+            Emp3.GetEmployee();
+
+            //with constructor
+            Employee1 emp1 = new Employee1(12122, 23, "Mohit");
+            emp1.GetEmployee();
+
+            Employee1 emp2 = new Employee1(112122, 23, "Pohit");
+            emp2.GetEmployee();
+
+            Employee1 emp3 = new Employee1(112122, 23, "Lohit");
+            emp3.GetEmployee();
+
+        }
+
+    }
+
+    //Without Constructor
+    class Employee
+    {
+        int EmpID;
+        int Age;
+        String Name;
+
+        public void SetEmployee(int empId, int age, String name)
+        {
+            EmpID = empId;
+            Age = age;
+            Name = name;
+        }
+
+        public void GetEmployee()
+        {
+            Console.WriteLine(EmpID);
+            Console.WriteLine(Age);
+            Console.WriteLine(Name);
+        }
+    }
+
+    //With Construtor
+    class Employee1
+    {
+        int EmpID;
+        int Age;
+        String Name;
+
+        public Employee1(int empId, int age, String name)
+        {
+            EmpID = empId;
+            Age = age;
+            Name = name;
+        }
+
+        public void GetEmployee()
+        {
+            Console.WriteLine(EmpID);
+            Console.WriteLine(Age);
+            Console.WriteLine(Name);
+        }
+    }
+}
---
+++ Chapter_01/Exercise1_03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Exercise1_03
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            var message = "Hello World";
+            FormatString(message);
+            Console.WriteLine(message);
+            var anotherMessage = "Welcome to World";
+            Console.WriteLine(FormatReturnString(anotherMessage));
+        }
+        public static void FormatString(String StringToFormat)
+        {
+            StringToFormat.Replace("World", "Mars");
+        }
+        public static String FormatReturnString(String StringToFormat)
+        {
+            return StringToFormat.Replace("World", "Mars");
+        }
+    }
+}
+//In C# String is immutable i.e we cannot change the value of string after its creation.
+//Because in C# string is treated like a arrays of characters. And Array having a Fixed Length to it.
+// thats why for "FormatString" it is not replacing "world" to "mars" but because of returning Void it is not able to replace.
+//Meanwhile "FormatReturnString" return a new value to a string.
+
+
+//output
+//Hello World
+//Welcome to Mars
